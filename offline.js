fa4fba446e11eb38be67957ffc8026b7d9eea418
edutekno <54890481+edutekno@users.noreply.github.com>
2022-02-25 12:51:07 +0700

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,10 +1,12 @@
 ﻿{
-	"version": 1645764869,
+	"version": 1645768205,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
+		"images/cute-sheet0.png",
+		"images/cute2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
